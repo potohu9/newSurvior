79e84dab5b44d9e27a561c4816ec8bf675555c75
WCCB <tfb00plzdj@gmail.com>
2014-10-10 15:15:00 +0900

    初期アップロード

---
+++ UnityAssets/KohakuChan/UnityUtileAssets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float spd = 1;
+	public float maxSpeed = 5;
+	public Vector3 vel = new Vector3(0,0,0);
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		bool isTouch = false;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended || touch.phase != TouchPhase.Canceled) {
+				// 現在のタッチ位置からRaycast
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				RaycastHit hit = new RaycastHit();
+				if (Physics.Raycast(ray,out hit)){
+					isTouch = true;
+					Vector3 targetPos = new Vector3(hit.point.x,hit.point.y,hit.point.z);
+					Vector3 sub = targetPos - transform.position;
+					Vector3 nor = sub.normalized;
+					vel = nor * spd;
+					vel.y = 0;
+				}
+			}
+		}
+		if(!isTouch){
+			vel.x = 0;
+			vel.z = 0;
+		}
+		rigidbody.AddForce(
+			vel,
+			ForceMode.Impulse);
+		if(rigidbody.velocity.magnitude > maxSpeed){
+			rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity,maxSpeed);
+		}
+	}
+}
---
+++ UnityAssets/KohakuChan/UnityUtileAssets/RectChenger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RectChenger {
+	
+	private static int windowLarge = 1280;
+	private static int windowSmall = 720;
+
+	public static Rect changeWindowRect(Rect rect){
+		Rect result;
+
+		int width;
+		int height;
+
+		if(Screen.width > Screen.height){
+			width = windowLarge;
+			height = windowSmall;
+		}
+		else{
+			width = windowSmall;
+			height = windowLarge;
+		}
+
+		float widthRatio = (float)Screen.width / (float)width;
+		float heightRatio = (float)Screen.height / (float)height;
+
+		result = new Rect(rect.x * widthRatio,rect.y * heightRatio,
+		                  rect.width * widthRatio,rect.height * heightRatio);
+
+		return result;
+	}
+}
