8eba91b0cefbf0476be8efd28eb4bcdf2f33ba00
potohu9 <meruhularia@gmail.com>
2014-10-31 13:23:31 +0900

    ステータスをスキルからいじれるよ
    きゃわたん

---
+++ SurviorProject/ステータス/PlayerStatus.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerStatus : MonoBehaviour {
+
+	public uint HP;
+	public uint AT;
+	public uint DF;
+	public uint SP;
+	Text atText;
+	Text dfText;
+	Text spText;
+
+	// Use this for initialization
+	void Start () {
+		HP = 100;
+		AT = 10;
+		DF = 10;
+		SP = 10;
+		GameObject obj = GameObject.Find("ATText");
+		atText = obj.GetComponent<Text>();
+		obj = GameObject.Find("DFText");
+		dfText = obj.GetComponent<Text>();
+		obj = GameObject.Find("SPText");
+		spText = obj.GetComponent<Text>();
+	}
+
+	void Update(){
+		atText.text = "AT" + AT;
+		dfText.text = "DF" + DF;
+		spText.text = "SP" + SP;
+	}
+}
---
+++ SurviorProject/ステータス/buttonest/HPmeterScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HPmeterScript : MonoBehaviour {
+
+	Image image;
+	GameObject obj;
+	PlayerStatus script;
+
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image>();
+		obj = GameObject.Find("unitychan");
+		script = obj.GetComponent<PlayerStatus> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float hp = (script.HP) * 0.01f;
+		image.fillAmount = hp;
+	}
+}
