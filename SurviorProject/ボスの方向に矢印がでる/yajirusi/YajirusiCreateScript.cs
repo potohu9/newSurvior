1285fed3d14d8e000d384ccb6fe5a998fead1f14
potohu9 <meruhularia@gmail.com>
2014-11-11 12:44:04 +0900

    矢印をファイルにしたよ

---
+++ SurviorProject/ボスの方向に矢印がでる/yajirusi/YajirusiCreateScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YajirusiCreateScript : MonoBehaviour {
+
+	GameObject yajirusi;
+	YajirusiScript1 script;
+	// Use this for initialization
+	void Start () {
+		yajirusi = Instantiate(Resources.Load("Prefabs/yajirusi/Yajirusi"),new Vector3(0, 0.1f, 0),Quaternion.identity) as GameObject;
+		
+		script = yajirusi.GetComponent<YajirusiScript1> ();
+		script.setTarget (this.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float rand = Random.value;
+		/*if(rand<=0.01){
+			Object.Destroy(this.gameObject);
+		}*/
+
+	}
+
+	void OnDestroy(){
+		Object.Destroy(yajirusi);
+	}
+}
---
+++ SurviorProject/ボスの方向に矢印がでる/yajirusi/YajirusiScript1.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YajirusiScript1 : MonoBehaviour {
+
+	Transform playerTransform;
+	Transform enemyTransform;
+	Transform yajirusiTransform;
+
+	public string target;
+
+	// Use this for initialization
+	void Start () {
+		GameObject player = GameObject.Find ("Player");
+		playerTransform = player.transform;
+
+		//GameObject enemy = GameObject.Find ("bossEnemy1").transform.FindChild("Query-Chan").gameObject;
+		//enemyTransform = enemy.transform;
+
+		yajirusiTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		yajirusiTransform.LookAt (enemyTransform.position);
+		yajirusiTransform.position = playerTransform.position;
+	}
+
+	public void setTarget(string t){
+		target = t;
+
+		GameObject enemy = GameObject.Find (target).transform.FindChild("Query-Chan").gameObject;
+		enemyTransform = enemy.transform;
+	}
+}
