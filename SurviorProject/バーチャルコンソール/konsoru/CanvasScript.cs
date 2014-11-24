1a0f80b5cc0be2f094e8497db07c4183d644b5f7
potohu9 <meruhularia@gmail.com>
2014-11-24 14:11:39 +0900

    コンソール左下版でけた

---
+++ SurviorProject/バーチャルコンソール/konsoru/CanvasScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasScript : MonoBehaviour {
+	Canvas canvas;
+	public Time timer;
+	private float timeS;
+	
+	// Use this for initialization
+	void Start () {
+		canvas = GetComponent<Canvas>();
+		canvas.renderMode = RenderMode.World;
+		timeS = 0;
+		timeS = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float timeN = Time.time;
+		if(timeN-timeS >= 1){
+			canvas.renderMode = RenderMode.Overlay;
+		}
+	}
+}
