5e8a10723e7b281d4fd3f42321a9bbd3c3dd158d
potohu9 <meruhularia@gmail.com>
2014-12-02 11:31:19 +0900

    敵の頭上のHP
    多分逝ける

---
+++ SurviorProject/エネミーのHP/EnemyHPCreateScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHPCreateScript : MonoBehaviour {
+
+	enemyHPScript enemyHP;
+	public bool tag = false;
+
+	// Use this for initialization
+	void Start () {
+		// プレハブを取得
+		GameObject prefab = (GameObject)Resources.Load ("enemyHPbass");
+		enemyHP = prefab.GetComponent<enemyHPScript> ();
+		enemyHP.setTarget (gameObject);
+		tag = true;
+		// プレハブからインスタンスを生成
+		Instantiate (prefab, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ SurviorProject/エネミーのHP/enemyHPScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyHPScript : MonoBehaviour {
+	GameObject canvas;
+	public GameObject trget;
+	public bool tag = false;
+	Camera camera;
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find ("enemyCanvas");
+		transform.parent = canvas.transform;
+		transform.localPosition = new Vector3 (0, 0, 0);
+
+		camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(trget!=null){
+			Vector3 ObjectPoint = trget.transform.position;
+
+			Vector3 screenPostion = camera.WorldToScreenPoint(ObjectPoint);
+
+			Vector3 pos = new Vector3(screenPostion.x-492, (Screen.height-screenPostion.y)*-1+350, 0);
+
+			transform.localPosition =pos;
+		}
+	}
+
+	public void setTarget(GameObject t){
+		//trget = GameObject.Find (t);
+		trget = t;
+		tag = true;
+	}
+}
