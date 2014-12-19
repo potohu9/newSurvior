6af7df041130a2e183fac14334d60d86fb823e27
twfurifuri <eu1i4sil@gmail.com>
2014-12-19 16:24:52 +0900

    メリークルシミマス

---
+++ SurviorProject/MoveScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets.CrossPlatformInput;
+public class MoveScript : MonoBehaviour {
+	//float spd = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 vec = new Vector3 (CrossPlatformInputManager.GetAxis("Horizontal"),0,CrossPlatformInputManager.GetAxis("Vertical"));
+		//transform.position += vec.normalized * spd;
+	}
+}
