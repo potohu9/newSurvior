6d7ece4c8417c8a3cf1df9947b1264b7ee164bcf
WCCB <tfb00plzdj@gmail.com>
2015-01-05 11:56:44 +0900

    伝言板的な何か更新
    更新しました

---
+++ test/dffUp.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class dffUp : SkillScript {
+
+ void Start () {
+  explanationText = "防御力強くなるよ";
+  invocationEffectPrefabPath = "res/prefab/dffeff";
+  runtimeEffectPrefabPath = "res/prefab/dffeff2";
+ }
+ 
+ //スキル効果記述欄
+ void skillEffect(GameObject targetObject){
+  
+ }
+}
