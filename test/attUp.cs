5e82f7689e967104b60a5de4a63a39c30b380018
potohu9 <meruhularia@gmail.com>
2015-01-08 10:29:51 +0900

    Revert "て"
    This reverts commit b4d064d65c24118a2cf282a19b9b088e94521995.

---
+++ test/attUp.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class attUp : SkillScript {
+
+ void Start () {
+  explanationText = "攻撃力強くなるよ";
+  invocationEffectPrefabPath = "res/prefab/atteff1";
+  runtimeEffectPrefabPath = "res/prefab/eff2";
+ }
+ 
+ //スキル効果記述欄
+ void skillEffect(GameObject targetObject){
+  
+ }
+}
